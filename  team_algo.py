f303e40caf4dfefbc4e34abd75354aa8dc39ff6a
hi1d <aopd408@gmail.com>
2021-12-20 20:07:07 +0900

    202112120

---  team_algo.py
+++  team_algo.py
@@ -35,20 +35,20 @@
 # 제한 사항
 # s는 길이 1 이상, 길이 8 이하인 문자열입니다.
 
-import re
-s = "123b4a"
+# import re
+# s = "123b4a"
 
 
-def solution(s):
-    if len(s) == 4 or len(s) == 6:
-        s_filter = re.sub('[^0-9]', '*', s)
-        if '*' in s_filter:
-            return False
-        return True
-    return False
+# def solution(s):
+#     if len(s) == 4 or len(s) == 6:
+#         s_filter = re.sub('[^0-9]', '*', s)
+#         if '*' in s_filter:
+#             return False
+#         return True
+#     return False
 
 
-print(solution(s))
+# print(solution(s))
 
 
 # 자연수 뒤집어 배열로 만들기
@@ -62,14 +62,14 @@ print(solution(s))
 # 12345	[5,4,3,2,1]
 
 
-n = 12345
+# n = 12345
 
 
-def solution(n):
-    return [i for i in str(n)[::-1]]
+# def solution(n):
+#     return [i for i in str(n)[::-1]]
 
 
-print(solution(n))
+# print(solution(n))
 
 
 # 가운데 글자 가져오기
@@ -83,8 +83,51 @@ print(solution(n))
 # "abcde"	    "c"
 # "qwer"	    "we"
 
-def solution(s):
-    return s[len(s)//2] if len(s) % 2 else s[len(s)//2-1]+s[len(s)//2]
+# def solution(s):
+#     return s[len(s)//2] if len(s) % 2 else s[len(s)//2-1]+s[len(s)//2]
 
 
-print(solution(s))
+# print(solution(s))
+
+# == == == == == == == == == == == == == == == == == == == == == == == == == ==
+# 핸드폰 번호 가리기
+# 문제 설명
+# 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+# 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한
+# 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+# 제한 조건
+# s는 길이 4 이상, 20이하인 문자열입니다.
+# 입출력 예
+# phone_number	return
+# "01033334444"	"*******4444"
+# "027778888"	"*****8888"
+import re
+# phone_number = "4444"
+
+
+# def solution(phone_number):
+#     if len(phone_number) == 4:
+#         return phone_number
+#     return re.sub('[0-9]', '*', phone_number, len(phone_number)-4)
+
+
+# print(solution(phone_number))
+
+# == == == == == == == == == == == == == == == == == == == == == == == == == ==
+
+# 나머지가 1이 되는 수 찾기
+# 문제 설명
+# 자연수 n이 매개변수로 주어집니다.
+# n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
+# 답이 항상 존재함은 증명될 수 있습니다.
+
+# 제한사항
+# 3 ≤ n ≤ 1, 000, 000
+# 입출력 예
+# n	result
+# 10	3
+# 12	11
+
+def solution(n):
+    return min([i for i in range(1, n) if n % i == 1])
--- 20211220.py
+++ 20211220.py
@@ -73,14 +73,145 @@ arr2 = [[2, 3, 4, 2], [5, 6, 7, 5], [8, 9, 1, 2]]
 # answer : [[4, 4, 4, 9], [10, 10, 10, 16], [5, 7, 9, 10]]
 
 
-def solution(arr1, arr2):
-    answer = [[] for i in range(len(arr1))]
+# def solution(arr1, arr2):
+#     answer = [[] for i in range(len(arr1))]
 
-    for i in range(len(arr1)):
-        for j in range(len(arr1[i])):
-            answer[i].append(arr1[i][j]+arr2[i][j])
+#     for i in range(len(arr1)):
+#         for j in range(len(arr1[i])):
+#             answer[i].append(arr1[i][j]+arr2[i][j])
+
+#     return answer
+
+
+# print(solution(arr1, arr2))
+
+# == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+# 콜라츠 추측
+# 문제 설명
+# 1937년 Collatz란 사람에 의해 제기된 이 추측은,
+# 주어진 수가 1이 될때까지 다음 작업을 반복하면,
+# 모든 수를 1로 만들 수 있다는 추측입니다.
+# 작업은 다음과 같습니다.
+
+# 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+# 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+# 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+# 예를 들어, 입력된 수가 6이라면 6→3→10→5→16→8→4→2→1 이 되어 총 8번 만에 1이 됩니다.
+# 위 작업을 몇 번이나 반복해야하는지 반환하는 함수, solution을 완성해 주세요.
+# 단, 작업을 500번을 반복해도 1이 되지 않는다면 –1을 반환해 주세요.
+
+# 제한 사항
+# 입력된 수, num은 1 이상 8000000 미만인 정수입니다.
+
+# n = 626331
+
+
+# def solution(num):
+#     result = 0
+#     while True:
+#         if result > 500:
+#             return -1
+#         if num != 1:
+#             if num % 2:
+#                 num = num*3 + 1
+#             else:
+#                 num = num/2
+#         else:
+#             return result
+#         result += 1
+#     return
+
+
+# print(solution(n))
+
+# == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+
+# 나머지가 1이 되는 수 찾기
+# 문제 설명
+# 자연수 n이 매개변수로 주어집니다.
+# n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를
+# return 하도록 solution 함수를 완성해주세요.
+# 답이 항상 존재함은 증명될 수 있습니다.
+
+# 제한사항
+# 3 ≤ n ≤ 1,000,000
+# 입출력 예
+# n	result
+# 10	3
+# 12	11
+
+# def solution(n):
+#     return min([i for i in range(1, n) if n % i == 1])
+
+
+# print(solution(10))
+# == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+# 제일 작은 수 제거하기
+# 문제 설명
+# 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수,
+# solution을 완성해주세요.
+# 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+# 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고,
+# [10]면 [-1]을 리턴 합니다.
+
+# 제한 조건
+# arr은 길이 1 이상인 배열입니다.
+# 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+# 입출력 예
+# arr	    return
+# [4,3,2,1]	[4,3,2]
+# [10]	    [-1]
+
+# arr = [4, 3, 2, 1]
+
+
+# def solution(arr):
+#     if len(arr) > 1:
+#         arr.pop(arr.index(min(arr)))
+#         return arr
+#     return [-1]
+
+
+# print(solution(arr))
+
+# == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+# 같은 숫자는 싫어
+# 문제 설명
+# 배열 arr가 주어집니다.
+# 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
+# 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+# 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다.
+
+# 예를 들면,
+
+# arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+# arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+# 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+# 제한사항
+# 배열 arr의 크기 : 1,000,000 이하의 자연수
+# 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+# 입출력 예
+# arr	answer
+# [1,1,3,3,0,1,1]	[1,3,0,1]
+# [4,4,4,3,3]	[4,3]
+
+arr = [4, 4, 4, 3, 3]
+
+
+def solution(arr):
+    answer = []
+
+    for i in arr:
+        if answer[-1:] == [i]:
+            continue
+        answer.append(i)
 
     return answer
 
 
-print(solution(arr1, arr2))
+print(solution(arr))
