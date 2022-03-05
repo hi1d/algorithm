088f9209c824e50738cdf4e363741b8ad7cec288
hi1d <aopd408@gmail.com>
2022-03-05 18:48:39 +0900

    2022.03.05

--- 20220304.py
+++ 20220304.py
@@ -10,8 +10,8 @@
 # 4   9   12  14
 # 10  11  15  16
 
-# r=4
-# c=6
+r=4
+c=6
 
 max_count = 8
 line_max = (max_count*2)-1
@@ -43,3 +43,22 @@ for n,i in enumerate(list):
 for i in result:
     print(i)
 print(result[r-1][c-1])        
+
+
+# =======================================================================
+row = []
+line = []
+list_len = len(list)
+for i in range(list_len):
+    sorted_list = sorted(list[i])
+    row.append(sorted_list[list_len//2])
+    line_line = []
+    for j in range(len(list)):
+        line_line.append(list[j][i])
+    line_line.sort()
+    line.append(line_line[list_len//2])
+
+result = 0
+for i in row:
+    if i in line:
+        result+=1
