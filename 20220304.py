82e11160466a0ed067cd4ba53d7dc77bb20b48da
hi1d <aopd408@gmail.com>
2022-03-08 10:47:16 +0900

    2022.03.08

--- 20220304.py
+++ 20220304.py
@@ -1,39 +1,39 @@
-# 1   2   6   7   15  
+# 1   2   6   7   15
 # 3   5   8   14  16
 # 4   9   13  17  22
 # 10  12  18  21  23
 # 11  19  20  24  25
 
 
-# 1   2   6   7 
+# 1   2   6   7
 # 3   5   8   13
 # 4   9   12  14
 # 10  11  15  16
 
-r=4
-c=6
+r = 4
+c = 6
 
 max_count = 8
 line_max = (max_count*2)-1
 count = 1
 count_max = 1
 list = []
-for i in range(1,line_max+1):
+for i in range(1, line_max+1):
     num_list = []
     if i > max_count:
-        count_max -=1
+        count_max -= 1
     else:
-        count_max +=1
-    for j in range(1,count_max):
+        count_max += 1
+    for j in range(1, count_max):
         num_list.append(count)
-        count+=1
-    if i%2:
+        count += 1
+    if i % 2:
         num_list.sort(reverse=True)
     list.append(num_list)
 
 list_count = 0
 result = [[]*max_count for i in range(max_count)]
-for n,i in enumerate(list):
+for n, i in enumerate(list):
     for j in range(len(i)):
         index = 0
         if n > (line_max//2):
@@ -42,23 +42,23 @@ for n,i in enumerate(list):
 
 for i in result:
     print(i)
-print(result[r-1][c-1])        
+print(result[r-1][c-1])
 
 
 # =======================================================================
-row = []
-line = []
-list_len = len(list)
-for i in range(list_len):
-    sorted_list = sorted(list[i])
-    row.append(sorted_list[list_len//2])
-    line_line = []
-    for j in range(len(list)):
-        line_line.append(list[j][i])
-    line_line.sort()
-    line.append(line_line[list_len//2])
+# row = []
+# line = []
+# list_len = len(list)
+# for i in range(list_len):
+#     sorted_list = sorted(list[i])
+#     row.append(sorted_list[list_len//2])
+#     line_line = []
+#     for j in range(len(list)):
+#         line_line.append(list[j][i])
+#     line_line.sort()
+#     line.append(line_line[list_len//2])
 
-result = 0
-for i in row:
-    if i in line:
-        result+=1
+# result = 0
+# for i in row:
+#     if i in line:
+#         result+=1
--- not_solve.py
+++ not_solve.py
@@ -149,3 +149,50 @@ limit = 100
 
 
 # print(solution(people, limit))
+
+
+
+# ===========================================================================
+
+# 문제 설명
+# 은퇴 후 치킨집 사장님이 된 종윤이는 고된 연습 끝에 1분에 2마리의 치킨을 튀길 수 있게 되었다. 이 소식을 들은 성구는 치킨집의 번창을 위해 X 마리의 치킨을 주문하였다.
+# 하지만 종윤이는 나이가 들어 혼자 모든 치킨을 튀길 수 없었고, 알바들을 고용할지 고민한다. 알바들은 한 명당 1분에 F 마리의 치킨을 튀길 수 있지만, 수당으로 치킨 C 마리를 미리 줘야 일을 하겠다고 한다. 성격이 급한 성구에게 혼나지 않으려면 가능한 빨리 치킨을 튀겨서 보내줘야 한다.
+# 종윤이가 알바들을 적절히 고용해서 성구가 주문한 X 마리의 치킨을 튀기는데 걸리는 최소 시간을 반환(return)하도록 solution 함수를 작성하시오. (반올림하여 소수점 6번째 자리까지의 값을 반환)
+# 입력
+# 알바 당 인건비(치킨): 1 <= C <= 10000
+# 알바 당 치킨 생산량: 1 <= F <= 100
+# 목표 치킨량: 1 <= X <= 100000
+# 입출력 예
+# C     F       X           Return
+# 30.0  1.0     2.0         1.0
+# 30.0  2.0     100.0       39.166667
+# 30.5  3.14159 1999.1999   63.968001
+# 500.0 4.0     2000.0      526.190476
+
+
+C = 30.0
+F = 2.0
+X = 100.0
+
+def no_alba(X):
+    return X/2.0
+
+def solution(C,F,X):
+    alone = no_alba(X)
+    together_min = 100000
+    for i in range(1,10):
+        alba = C/((F*(i-1))+2.0)
+        print(alba)
+        order = X / F*(i+2.0)
+        print(order)
+        together = alba + order
+        print(together)
+        print("----------------")
+        # together = (C*i)/2.0 + X/(F*i+2.0)
+        # print(together)
+        # if together_min > together:
+        #     together_min = together
+    return
+    # return round(min(alone, together_min),6)
+
+print(solution(C,F,X))
