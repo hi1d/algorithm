e0410e17b6d2dbc3ed07d87fab5743d354f267f4
hi1d <aopd408@gmail.com>
2022-04-04 18:05:44 +0900

    2022-04-04

--- 20220404.py
+++ 20220404.py
@@ -94,22 +94,56 @@
 # 1
 # 0
 
+# import sys
+# import math
+
+# input = sys.stdin.readline
+# T = int(input())
+
+# for _ in range(T):
+#     x1, y1, r1, x2, y2, r2 = map(int, input().split())
+
+#     distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+#     if distance == 0 and r1 == r2:
+#         print(-1)
+#     elif abs(r1-r2) == distance or abs(r1+r2) == distance:
+#         print(1)
+#     elif abs(r1-r2) < distance < r1+r2:
+#         print(2)
+#     else:
+#         print(0)
+
+# 문제
+# 어떤 자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다. 
+# 어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다. 
+# 예를 들어, 245의 분해합은 256(=245+2+4+5)이 된다. 
+# 따라서 245는 256의 생성자가 된다. 
+# 물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다. 
+# 반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
+
+# 자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+
+# 출력
+# 첫째 줄에 답을 출력한다. 생성자가 없는 경우에는 0을 출력한다.
+
+# 예제 입력 1 
+# 216
+# 예제 출력 1 
+# 198
+
 import sys
-import math
 
 input = sys.stdin.readline
-T = int(input())
+N = int(input())
+result = 0
+for i in range(1, N+1):
+    num = sum(list(map(int, str(i))))
+    if i+num == N:
+        result = i
+        break
 
-for _ in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-
-    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-    if distance == 0 and r1 == r2:
-        print(-1)
-    elif abs(r1-r2) == distance or abs(r1+r2) == distance:
-        print(1)
-    elif abs(r1-r2) < distance < r1+r2:
-        print(2)
-    else:
-        print(0)
+print(result)
